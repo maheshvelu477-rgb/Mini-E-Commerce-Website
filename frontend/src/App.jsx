7e2d511873d0db236d1bdd15a7798550ff2597a7
umauma4846-collab <umauma4846@gmail.com>
2025-12-26 13:39:27 +0530

    Initial Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer,toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Nav from "./components/Nav";
 import ProductList from "./components/ProductList";
